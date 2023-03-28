1ecee8b478a2162c4eb58ff3e3bda5e1cb2e8237
NayemulSCode <nayemulhasan2016@gmail.com>
2023-03-29 00:24:47 +0600

    29-03-2023 array reduce function implement

--- play-with-dom-arrray/script.js
+++ play-with-dom-arrray/script.js
@@ -3,6 +3,7 @@ const main = document.getElementById('main');
 const double = document.getElementById('double');
 const millionaires = document.getElementById('show-millionaires');
 const sort = document.getElementById('sort');
+const calculate_wealth = document.getElementById('calculate-wealth');
 let data = [];
 getRandomUser();
 
@@ -11,6 +12,7 @@ async function getRandomUser(){
     const res = await fetch('https://randomuser.me/api/');
     const data = await res.json();
     const user = data.results[0];
+    // console.log(" getRandomUser ~ user:", user)
     const userInfo ={
         name: `${user.name.title} ${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random()*100000)
@@ -24,11 +26,13 @@ function addUser(obj){
     udpateDOM()
 }
 
+
+
 // insert into dom
 
 function udpateDOM(provideData = data){
-    console.log("provided",provideData)
-    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+    // console.log("provided",provideData)
+    main.innerHTML= '<h2><strong>Person</strong> Wealth</h2>';
     provideData.forEach(item=>{
         const element = document.createElement('div');
         element.classList.add('person');
@@ -37,7 +41,25 @@ function udpateDOM(provideData = data){
     });
 };
 
+let sum = 0;
+function summation() {
+    data.forEach((user) => {
+        sum = sum + user.money;
+    });
+    udpateDOM(data, sum);
+}
+// calculate wealthCalulate.
+function calculateWealth(){
+   let sum  = data.reduce((a, user)=> {
+       console.log("a=", a, "user=", user)
+       return a + user.money;
+    },0);
+    const wealthEle = document.createElement('div');
+    wealthEle.innerHTML = `Total wealth= ${sum}`;
+    main.appendChild(wealthEle);
+}
 // double money
+
 function doubleMoney(){
     data = data.map(user =>{
         return {
@@ -66,8 +88,20 @@ addUserBtn.addEventListener('click',getRandomUser);
 double.addEventListener('click', doubleMoney);
 millionaires.addEventListener('click', showMillionaires);
 sort.addEventListener('click', sortByRichest);
+calculate_wealth.addEventListener('click', calculateWealth);
 
 // let exam =  '{"name":"John", "age":30, "car":null}'
 // let parseValue = JSON.parse(exam)
 // console.log("json parse",  JSON.parse(exam));
 // console.log("json", typeof JSON.stringify(parseValue))
+
+const numbers = [1, 5, 2, 7, 3];
+const largest = numbers.reduce((accumulator, currentValue) => {
+    // console.log(accumulator, currentValue)
+  if (currentValue > accumulator) {
+    return currentValue;
+  } else {
+    return accumulator;
+  }
+},0);
+// console.log(largest);
