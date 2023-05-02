8fa1b2e66f71471868d6542da658ed18680e2fcd
NayemulSCode <nayemulhasan2016@gmail.com>
2023-05-02 22:46:18 +0600

    form data and store data in localstorage

--- card-of-question-ans/script.js
+++ card-of-question-ans/script.js
@@ -2,7 +2,28 @@ const showBtn = document.getElementById('show');
 const hideBtn = document.getElementById('hide');
 const addContainer = document.getElementById('add-container');
 
+const addCardBtn = document.getElementById('add-card');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+
 // Show add container
 showBtn.addEventListener('click', () => addContainer.classList.add('show'));
 // Hide add container
 hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+//add local storage
+function setCardsData(cards){
+    localStorage.setItem('cards', JSON.stringify(cards))
+}
+
+addCardBtn.addEventListener('click', ()=>{
+    const question = questionEl.value;
+    const answer = answerEl.value;
+    if(question.trim() && answer.trim()){
+        console.log('qsn-- if', question.trim())
+        console.log('ans-- if', answer.trim())
+        const newObj = { question, answer };
+        console.log('ans-- if', newObj)
+        setCardsData(newObj)
+    }
+})
